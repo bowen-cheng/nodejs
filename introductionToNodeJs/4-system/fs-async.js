5ce43c938cb8a94a8d4116c940010a51d3840e05
Bowen Cheng <blueicycky@gmail.com>
2017-09-23 11:15:09 -0400

    Practise the fs (file system) module
    The fs-async.js demonstrates the "Christmas tree" problem which involves
    chaining too many callback functions. This should be avoided by using
    more synchronous calls.

---
+++ introductionToNodeJs/4-system/fs-async.js
@@ -0,0 +1,37 @@
+var fs = require("fs");
+
+const content = "This file and its content is written using NodeJs!";
+const testDir = "temp/";
+const oldFile = "old-name.txt";
+const newFile = "new-name.txt";
+const newFilePath = testDir + newFile;
+
+console.log("Cleaning up any residual files from previous runs...");
+if (fs.existsSync(testDir)) {
+  if (fs.existsSync(newFilePath)) {
+    fs.unlinkSync(newFilePath);
+  }
+  fs.rmdirSync(testDir);
+}
+console.log("Clean up complete");
+
+// These chained callback functions demonstrates the "Christmas tree problem",
+// which should be avoided by using synchronous calls
+
+fs.mkdir(testDir, function (err) {
+  fs.exists(testDir, function (exists) {
+    if (exists) {
+      process.chdir(testDir);
+      fs.writeFile(oldFile, content, function (err) {
+        fs.rename(oldFile, newFile, function (err) {
+          fs.stat(newFile, function (err, stats) {
+            console.log("File size: " + stats.size + " bytes");
+            fs.readFile(newFile, function (err, data) {
+              console.log("File content: " + data.toString());
+            })
+          })
+        })
+      })
+    }
+  })
+});
---
+++ introductionToNodeJs/4-system/fs-sync.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+
+const content = "This file and its content is written using NodeJs!";
+const testDir = "temp/";
+const oldFile = "old-name.txt";
+const newFile = "new-name.txt";
+const newFilePath = testDir + newFile;
+
+console.log("Checking if the directory 'temp exists'...");
+
+// The "sync" suffix of the function names indicates they are synchronous
+if (fs.existsSync(testDir)) {
+  console.log("The directory 'temp' exists, cleaning up...");
+  if (fs.existsSync(newFilePath)) {
+    fs.unlinkSync(newFilePath);
+  }
+  fs.rmdirSync(testDir);
+  console.log("The directory 'temp' and files in it are now removed");
+} else {
+  console.log("Making directory 'temp'...");
+  fs.mkdirSync(testDir);
+}
+
+if (fs.existsSync(testDir)) {
+  process.chdir(testDir);
+  fs.writeFileSync(oldFile, content);
+  fs.renameSync(oldFile, newFile);
+  console.log("File size: " + fs.statSync(newFile).size + " bytes");
+  console.log("File content: " + fs.readFileSync(newFile).toString());
+}
