a277c6671b4c7526283b2d43fd34c9f52782f30a
Bowen Cheng <blueicycky@gmail.com>
2017-09-24 17:17:49 -0400

    Add the missing text file, host and port to the Node server

--- introductionToNodeJs/5-web/server.js
+++ introductionToNodeJs/5-web/server.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 
-const localFilePath = __dirname + "file.text";
-var port = "unknown";
-var ip = "unknown";
+const localFilePath = __dirname + "/test-file.txt";
+const port = 8080;
+const ip = "localhost";
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
@@ -12,8 +12,6 @@ http.createServer(function (request, response) {
   } else {
     response.end("Hello from a Node server");
   }
-  ip = process.env.IP;
-  port = process.env.PORT;
-}).listen(process.env.PORT, process.env.IP);
+}).listen(port, ip);
 
 console.log("Server is now running at: " + ip + ":" + port);
